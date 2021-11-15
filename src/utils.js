b9f723fe86618ec40a0d51da26b3fcc6ab02646f
Polina Antonova <panyasnap@mail.ru>
2021-11-15 13:22:51 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-export const base_url = 'https://data.fixer.io/api/latest?';
+export const base_url = 'http://data.fixer.io/api/latest?';
 export  const api_key = '77a894ed2174a20dc32cbc396e273eb1'
 export  const url = `${base_url}access_key=${api_key}`
 
